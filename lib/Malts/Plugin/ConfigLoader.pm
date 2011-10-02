b1c1d2d1d12a414f3272b622ed1da8bd566098eb
hisaichi5518 <info@moe-project.com>
2011-10-02 20:12:45 +0900

    clean up

--- lib/Malts/Plugin/ConfigLoader.pm
+++ lib/Malts/Plugin/ConfigLoader.pm
@@ -8,7 +8,7 @@ use Log::Minimal qw(croakf);
 sub init {
     my ($class, $c, $opts) = @_;
 
-    my $mode = $opts->{mode} || $c->mode || '';
+    my $mode = $opts->{mode} || $c->mode;
     my $fname = File::Spec->catfile($c->app_dir, 'config', "$mode.pl");
     $c->{config} = do $fname or croakf "Cannot load configuration file: $fname";
 }
@@ -26,7 +26,7 @@ __END__
 
 app_dir/config以下に設定ファイルを置く。
 
-設定ファイル名は、PLACK_ENVまたはオプションのmodeで指定する。
+設定ファイル名は、$ENV{PLACK_ENV}またはオプションのmodeで指定する。
 
 何も指定されていない場合は、development.plが読み込まれる。
 
