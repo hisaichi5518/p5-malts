8f7b85991fced1e734ebd5fe1d3e8cbaf359c7ab
hisaichi5518 <info@moe-project.com>
2011-12-03 18:01:32 +0900

    app_base_classがなかった場合のエラーメッセージを変更した

--- lib/Malts.pm
+++ lib/Malts.pm
@@ -20,7 +20,7 @@ sub new {
 
 # copied Amon2::Util::base_dir
 sub app_dir {
-    my $path = $_[0]->app_base_class or croakf 'You MUST set app_base_class. see Malts#app_base_class document.';
+    my $path = $_[0]->app_base_class or croakf 'You MUST set MyApp#app_base_class. see Malts#app_base_class document.';
     $path =~ s!::!/!g;
 
     if (my $libpath = $INC{"$path.pm"}) {
